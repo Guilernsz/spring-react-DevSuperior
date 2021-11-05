a60ad138e64324f51badbbca7e3fb39df58b4f98
Guilernsz <srvoltaire123@gmail.com>
2021-11-05 02:08:52 -0300

    Group by search

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleSumDTO.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SaleSumDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String sellerName;
+	private Double sum;
+	
+	public SaleSumDTO() {
+	}
+
+	public SaleSumDTO(Seller seller, Double sum) {
+		this.sellerName = seller.getName();
+		this.sum = sum;
+	}
+	
+	
+
+}
